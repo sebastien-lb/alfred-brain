7c5db4f303e7009e7911de203c11dd11c2e98a24
Sebastien <sebast.lubineau@laposte.net>
2018-11-26 16:04:10 +0100

    feat(app): add object collector app

--- alfredbrain/settings.py
+++ alfredbrain/settings.py
@@ -37,9 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'object_collector'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +52,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 ROOT_URLCONF = 'alfredbrain.urls'
 
@@ -122,3 +134,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# CORS HEADER 
+CORS_ORIGIN_ALLOW_ALL = True   
---
+++ object_collector/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ObjectCollectorConfig(AppConfig):
+    name = 'object_collector'
