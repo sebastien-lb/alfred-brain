939a40f7ec769cab71d47c0ada4a28a8b3d53c3d
Clement Dessoude <clement.dessoude@gmail.com>
2018-12-08 18:10:17 +0100

    feat(serializers): add category to list of fields returned by smart object's serializer

--- object_collector/serializers.py
+++ object_collector/serializers.py
@@ -2,31 +2,36 @@ from rest_framework import serializers
 
 from .models import Action, CategoryType, DataPoint, DataPollingType, DataSource, DataType, PerformedAction, SmartObject
 
-# Serializers define the API representation.
+
 class SmartObjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = SmartObject
-        fields = ('name','address_ip','port','id')
+        fields = ('id', 'name', 'address_ip', 'port', 'category')
+
 
 class ActionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Action
         fields = ('name', 'command', 'payload', 'smart_object','id')
 
+
 class DataPointSerializer(serializers.ModelSerializer):
     class Meta:
         model = DataPoint
         fields = ('id','value','data_source','timestamp')
+
 
 class PerformedActionSerializer(serializers.ModelSerializer):
     class Meta:
         model = PerformedAction
         fields = ('id','action','timestamp')
 
+
 class DataSourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = DataSource
         fields = ('id', 'name', 'description', 'data_type', 'endpoint', 'entrypoint', 'data_polling_type', 'smart_object')
+
 
 # reference types
 
@@ -35,13 +40,14 @@ class DataTypeSerializer(serializers.ModelSerializer):
         model = DataType
         fields = ('name',)
 
+
 class DataPollingTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = DataPollingType
         fields = ('name',)
 
+
 class CategoryTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = CategoryType
         fields = ('name',)
-
