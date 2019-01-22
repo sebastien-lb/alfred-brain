c03beb788f03e7b244dbc2e4646373dfe9c23c57
Clement Dessoude <clement.dessoude@gmail.com>
2019-01-22 23:28:24 +0100

    fix(scenarios): fix binary conversion in perform action
    (and add logging)

--- object_collector/logic/actionUtils.py
+++ object_collector/logic/actionUtils.py
@@ -3,9 +3,12 @@ import requests
 from object_collector.models import Action
 from object_collector.serializers import PerformedActionSerializer
 
+import logging
+logger = logging.getLogger('django')
 
 def performAction(action_id, payload):
     # execute action
+    logger.info("Execute action %s with payload %s" % (action_id, payload))
     action = Action.objects.get(pk=action_id)
     url = 'http://' + action.smart_object.address_ip + ":" + action.smart_object.port + action.command
     try:
