b58335f6434c42680f6a35bf6cf3e00d79eb44be
sebastien-lb <sebast.lubineau@laposte.net>
2019-02-21 14:15:57 +0100

    feat(install): ask user about his folder

--- install_rpi.sh
+++ install_rpi.sh
@@ -4,12 +4,44 @@
 BRAIN_FOLDER="brain"
 FACE_FOLDER="face"
 
-echo Deleting previous folder
-rm -rf $BRAIN_FOLDER $FACE_FOLDER
+# echo Deleting previous folder
+# rm -rf $BRAIN_FOLDER $FACE_FOLDER
 
-echo Fetching repos from git
-git clone https://github.com/sebastien-lb/alfred-brain.git $BRAIN_FOLDER
-git clone https://github.com/sebastien-lb/alfred-face.git $FACE_FOLDER
+echo "Fetching repos from git"
+if [ -d "$BRAIN_FOLDER" ]; then
+    echo "A folder named $BRAIN_FOLDER already exists"
+    echo "Do you wish to remove it? (y/n)"
+    read -r -p "" yn
+    case $yn in
+        [Yy]* ) echo "removing it.."; 
+                rm -rf $BRAIN_FOLDER; 
+                git clone https://github.com/sebastien-lb/alfred-brain.git $BRAIN_FOLDER; 
+                break;;
+        [Nn]* ) echo "Not removing it, exiting script"; exit;;
+        * ) echo "Please answer yes or no."; exit;;
+    esac
+else 
+    git clone https://github.com/sebastien-lb/alfred-brain.git $BRAIN_FOLDER
+fi
+
+if [ -d "$FACE_FOLDER" ]; then
+    echo "A folder named $FACE_FOLDER already exists"
+    echo "Do you wish to remove it? (y/n)"
+    read -r -p "" yn
+    case $yn in
+        [Yy]* ) echo "removing it.."; 
+                rm -rf $FACE_FOLDER; 
+                git clone https://github.com/sebastien-lb/alfred-face.git $FACE_FOLDER; 
+                break;;
+        [Nn]* ) echo "Not removing it, exiting script"; exit;;
+        * ) echo "Please answer yes or no."; exit;;
+    esac
+else 
+    git clone https://github.com/sebastien-lb/alfred-face.git $FACE_FOLDER
+fi
+
+# git clone https://github.com/sebastien-lb/alfred-brain.git $BRAIN_FOLDER
+# git clone https://github.com/sebastien-lb/alfred-face.git $FACE_FOLDER
 
 # installing deps 
 
@@ -37,15 +69,28 @@ else
     fi
 fi 
 
+install_deps() {
+    echo "installing repo deps"
+    cd $FACE_FOLDER
+    npm install
+    cd ../$BRAIN_FOLDER
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+    deactivate
+    cd ..
+}
+    
 
-echo installing repo deps
-cd $FACE_FOLDER 
-npm install
-cd ..
-cd ../$BRAIN_FOLDER
-python -m venv venv
-source venv/bin/activate
-pip install r requirements.txt
+echo "Do you want to install projet dependencies? (y/n)"
+read -r -p "" yn
+case $yn in
+    [Yy]* ) echo "installing..."; install_deps; break;;
+    [Nn]* ) echo "Not installing deps"; exit;;
+    * ) echo "Please answer yes or no."; exit;;
+esac
+
+
 
 
 
